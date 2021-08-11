8f1638687420e45ac0ac5b7d5955f4ac3aaccc77
hyejinzz <hyejinj810@gmail.com>
2021-08-11 23:09:58 +0900

    유저 update 주석

--- src/main/java/com/semobook/job/InactiveUserJobConfig.java
+++ src/main/java/com/semobook/job/InactiveUserJobConfig.java
@@ -1,79 +1,80 @@
-package com.semobook.job;
+//package com.semobook.job;
+//
+//import com.semobook.domain.User;
+//import com.semobook.repository.UserRepository;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.batch.core.Job;
+//import org.springframework.batch.core.Step;
+//import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+//import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+//import org.springframework.batch.core.configuration.annotation.StepScope;
+//import org.springframework.batch.item.ItemProcessor;
+//import org.springframework.batch.item.ItemWriter;
+//import org.springframework.batch.item.support.ListItemReader;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.time.LocalDate;
+//import java.util.List;
+//
+//
+//@Slf4j
+//@RequiredArgsConstructor
+//@Configuration
+//public class InactiveUserJobConfig {
+//    private final JobBuilderFactory jobBuilderFactory; // 생성자 DI 받음
+//    private final StepBuilderFactory inactiveUserStep; // 생성자 DI 받음
+//
+//    private final UserRepository userRepository;
+//
+//
+//    @Bean
+//    public Job inactiveUserJob(
+//            JobBuilderFactory jobBuilderFactory,
+//            Step inactiveJobStep
+//    ) {
+//        log.info("********** This is inactiveUserJob");
+//        return jobBuilderFactory.get("inactiveUserJob")
+//                .preventRestart()
 
-import com.semobook.domain.User;
-import com.semobook.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.support.ListItemReader;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.time.LocalDate;
-import java.util.List;
-
-
-@Slf4j
-@RequiredArgsConstructor
-@Configuration
-public class InactiveUserJobConfig {
-    private final JobBuilderFactory jobBuilderFactory; // 생성자 DI 받음
-    private final StepBuilderFactory inactiveUserStep; // 생성자 DI 받음
-
-    private final UserRepository userRepository;
-
-
-    @Bean
-    public Job inactiveUserJob(
-            JobBuilderFactory jobBuilderFactory,
-            Step inactiveJobStep
-    ) {
-        log.info("********** This is inactiveUserJob");
-        return jobBuilderFactory.get("inactiveUserJob")
-                .preventRestart()
-                .start(inactiveJobStep)
-                .build();
-    }
-
-
-    @Bean
-    public Step inactiveJobStep(
-            StepBuilderFactory stepBuilderFactory
-    ) {
-        log.info("********** This is inactiveUserStep");
-        return stepBuilderFactory.get("inactiveUserStep")
-                .<User, User> chunk(3)
-                .reader(inactiveUserReader())
-                .processor(inactiveUserProcessor())
-                .writer(inactiveUserWriter())
-                .build();
-    }
-
-
-    @Bean
-    @StepScope
-    public ListItemReader<User> inactiveUserReader() {
-        List<User> oldUsers = userRepository.findAllByLastConnectionBefore(LocalDate.now().minusMonths(3));
-        return new ListItemReader<>(oldUsers);
-    }
-
-    public ItemProcessor<User, User> inactiveUserProcessor() {
-        log.info("========memberProcessor========");
-        return User::setIncative;
-    }
-
-    //저장
-    public ItemWriter<User> inactiveUserWriter() {
-        log.info("========memberWriter========");
-
-        return ((List<? extends User> users) ->
-                userRepository.saveAll(users));
-
-    }
-}
+//                .start(inactiveJobStep)
+//                .build();
+//    }
+//
+//
+//    @Bean
+//    public Step inactiveJobStep(
+//            StepBuilderFactory stepBuilderFactory
+//    ) {
+//        log.info("********** This is inactiveUserStep");
+//        return stepBuilderFactory.get("inactiveUserStep")
+//                .<User, User> chunk(3)
+//                .reader(inactiveUserReader())
+//                .processor(inactiveUserProcessor())
+//                .writer(inactiveUserWriter())
+//                .build();
+//    }
+//
+//
+//    @Bean
+//    @StepScope
+//    public ListItemReader<User> inactiveUserReader() {
+//        List<User> oldUsers = userRepository.findAllByLastConnectionBefore(LocalDate.now().minusMonths(3));
+//        return new ListItemReader<>(oldUsers);
+//    }
+//
+//    public ItemProcessor<User, User> inactiveUserProcessor() {
+//        log.info("========memberProcessor========");
+//        return User::setIncative;
+//    }
+//
+//    //저장
+//    public ItemWriter<User> inactiveUserWriter() {
+//        log.info("========memberWriter========");
+//
+//        return ((List<? extends User> users) ->
+//                userRepository.saveAll(users));
+//
+//    }
+//}
--- src/main/java/com/semobook/scheduler/UserScheduler.java
+++ src/main/java/com/semobook/scheduler/UserScheduler.java
@@ -1,34 +1,65 @@
-//package com.semobook.scheduler;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.batch.core.Job;
-//import org.springframework.batch.core.JobExecutionException;
-//import org.springframework.batch.core.JobParametersBuilder;
-//import org.springframework.batch.core.launch.JobLauncher;
-//import org.springframework.scheduling.annotation.Scheduled;
-//import org.springframework.stereotype.Component;
-//
-//import java.time.LocalDateTime;
-//
-//@Component
-//@RequiredArgsConstructor
-//public class UserScheduler {
-//
-//    private final Job job;
-//    private final JobLauncher jobLauncher;
-//
-//    @Scheduled(fixedDelay = 5 * 1000L)
-//    public void executeJob () {
-//        try {
-//            jobLauncher.run(
-//                    job,
-//                    new JobParametersBuilder()
-//                            .addString("datetime", LocalDateTime.now().toString())
-//                            .toJobParameters()
-//            );
-//        } catch (JobExecutionException ex) {
-//            System.out.println(ex.getMessage());
-//            ex.printStackTrace();
-//        }
-//    }
-//}
+package com.semobook.scheduler;
+
+import com.semobook.job.UpdateRecomBasedUserInfoConfiguration;
+import com.semobook.job.UpdateRecomByBookConfiguration;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class UserScheduler {
+
+    private final JobLauncher jobLauncher;
+    private final UpdateRecomBasedUserInfoConfiguration updateRecomBasedUserInfoConfiguration;
+    private final UpdateRecomByBookConfiguration updateRecomByBookConfiguration;
+
+    @Scheduled(initialDelay = 1000, fixedDelay = 1000*60)
+//    @Scheduled(cron = "0 40 1 * * ?") 매일 한시 사십분에 돌린다
+
+    public void updateRecomBasedUserInfoJob () {
+        JobExecution execution;
+        try {
+            log.info("start updateRecomBasedUserInfoJob");
+//            execution = jobLauncher.run(updateRecomBasedUserInfoConfiguration.updateRecomBasedUserInfoJob(),simpleJobParam());
+//            log.info("Job finished with status : " + execution.getStatus());
+            log.info("Current Thread: {}",Thread.currentThread().getName());
+
+        } catch (Exception e){
+            log.error(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+
+    //    @Scheduled(cron = "0 0 2 * * ?") 매일 두시에 돌린다
+   public void updateRecomByBookJob(){
+       JobExecution execution;
+       try {
+           log.info("start updateRecomByBook");
+//           execution = jobLauncher.run(updateRecomByBookConfiguration.updateRecomByBookJob(),simpleJobParam());
+//           log.info("Job finished with status : " + execution.getStatus());
+           log.info("Current Thread: {}",Thread.currentThread().getName());
+
+       } catch (Exception e){
+           log.error(e.getMessage());
+           e.printStackTrace();
+       }
+
+   }
+
+   //같은 이름의 batch는 생길 수 없기 때문에 param에 시간을 넣는다.
+    private JobParameters simpleJobParam() {
+        Map<String, JobParameter> confMap = new HashMap<>();
+        confMap.put("time", new JobParameter(System.currentTimeMillis()));
+        return new JobParameters(confMap);
+    }
+}
